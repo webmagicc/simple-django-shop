7527774020bc8a4f971f485fc2dbac10d632d037
vladimir.dominiom <webmagic@ukr.net>
2017-02-15 10:43:28 +0200

    add property parser

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import CategoryProperty, ProductProperty
 
-# Register your models here.
+
+class ProductPropertyInline(admin.TabularInline):
+    model = ProductProperty
+    extra = 1
+    verbose_name_plural = 'Values'
+    suit_classes = 'suit-tab suit-tab-values'
+
+
+@admin.register(CategoryProperty)
+class CategoryPropertyAdmin(admin.ModelAdmin):
+    inlines = [ProductPropertyInline,]
+    suit_form_tabs = (('general', 'General'), 
+        ('values', 'Values'), )
+    fieldsets = [
+        ('General', {
+            'classes': ('suit-tab', 'suit-tab-general',),
+            'fields': [
+             'name',
+             'category',
+             'published',
+             'ordering',
+             ]
+        }),
+    ]
+
+
+@admin.register(ProductProperty)
+class ProductPropertyAdmin(admin.ModelAdmin):
+    pass
